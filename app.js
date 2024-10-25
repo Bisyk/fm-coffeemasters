82632fc1b2553af349e1a9ff2ff3bdab0d824cf6
Bisyk <99905134+Bisyk@users.noreply.github.com>
2024-10-26 00:05:41 +0300

    finish "Routing" module

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import Store from './services/Store.js'
 import API from './services/API.js'
+import Router from './services/Router.js'
 import { loadData } from './services/Menu.js'
 
-window.app = {
-  store: Store
-}
+window.app = {}
+
+app.store = Store
+app.router = Router
 
 window.addEventListener('DOMContentLoaded', async () => {
   loadData()
+  app.router.init()
 })
